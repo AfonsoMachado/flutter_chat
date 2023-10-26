9189b2058b66ed710472cca2fef4a7b84e636735
Afonso Machado <afonsosmachado@gmail.com>
2023-10-26 10:23:12 -0300

    first commit

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const RefreshProgressIndicator(),
+            const SizedBox(height: 10),
+            Text(
+              'Carregando...',
+              style: TextStyle(
+                  color: Theme.of(context).primaryTextTheme.titleLarge?.color),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
