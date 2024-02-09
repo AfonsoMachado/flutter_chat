447b762c989c0cab871bc6f6b17037b05967cc9d
Afonso Machado <afonsosmachado@gmail.com>
2024-02-09 11:45:28 -0300

    auth form adjusts

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/models/auth_form_data.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    _formKey.currentState?.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +23,43 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
+              if (_formData.isSignUp)
+                TextFormField(
+                  key: const ValueKey('name'),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Nome'),
-              ),
-              TextFormField(
+                key: const ValueKey('email'),
+                initialValue: _formData.email,
+                onChanged: (email) => _formData.email = email,
                 decoration: const InputDecoration(labelText: 'E-mail'),
               ),
               TextFormField(
+                key: const ValueKey('password'),
+                initialValue: _formData.password,
+                onChanged: (password) => _formData.password = password,
                 obscureText: true,
                 decoration: const InputDecoration(labelText: 'Senha'),
               ),
               const SizedBox(height: 12),
-              ElevatedButton(onPressed: () {}, child: const Text('Entrar')),
-              TextButton(onPressed: () {}, child: const Text('Criar um nova conta?'))
+              ElevatedButton(
+                  onPressed: _submit,
+                  child: Text(_formData.isSignIn ? 'Entrar' : 'Cadastrar')),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _formData.toggleAuthMode();
+                  });
+                },
+                child: Text(_formData.isSignIn
+                    ? 'Criar um nova conta?'
+                    : 'Já possui conta?'),
+              )
             ],
           ),
         ),
