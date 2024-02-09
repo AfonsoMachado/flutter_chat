7678f2a7b00d41f1128a0b6a4d631448f8de8ee6
Afonso Machado <afonsosmachado@gmail.com>
2024-02-09 17:24:42 -0300

    create message component to show the messages

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/core/models/chat_message.dart';
+import 'package:flutter_chat/core/services/chat/chat_service.dart';
 
 class Messages extends StatelessWidget {
   const Messages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mensagens'),
-    );
+    return StreamBuilder<List<ChatMessage>>(
+        stream: ChatService().messagesStream(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('Sem dados. Vamos conversar?'));
+          } else {
+            final msgs = snapshot.data!;
+            return ListView.builder(
+              reverse: true,
+              itemCount: msgs.length,
+              itemBuilder: (ctx, i) => Text(msgs[i].text),
+            );
+          }
+        });
   }
 }
