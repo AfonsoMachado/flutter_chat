49c0e59028289ec2253c613e1bed5103a34c13b9
Afonso Machado <afonsosmachado@gmail.com>
2024-02-09 15:23:46 -0300

    add default user

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -6,13 +6,22 @@ import 'package:flutter_chat/core/models/chat_user.dart';
 import 'package:flutter_chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static const _defaultUser = ChatUser(
+    id: '1',
+    name: 'Teste',
+    email: 'teste@gmail.com',
+    imageUrl: 'assets/images/avatar.png',
+  );
+
   // Atributos que pertencem à classe e não a instancia
-  static final Map<String, ChatUser> _users = {};
+  static final Map<String, ChatUser> _users = {
+    _defaultUser.email: _defaultUser
+  };
   static ChatUser? _currentUser;
   static MultiStreamController<ChatUser?>? _controller;
   static final _userStream = Stream<ChatUser?>.multi((controller) {
     _controller = controller;
-    _updateUser(null);
+    _updateUser(_defaultUser);
   });
 
   @override
