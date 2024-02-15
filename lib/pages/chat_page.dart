1199560e02d7328d71ba7e397eed952607f63d5a
Afonso Machado <afonsosmachado@gmail.com>
2024-02-15 11:36:51 -0300

    init notification page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat/components/messages.dart';
 import 'package:flutter_chat/components/new_message.dart';
 import 'package:flutter_chat/core/services/auth/auth_service.dart';
+import 'package:flutter_chat/core/services/notification/chat_notification_service.dart';
+import 'package:flutter_chat/pages/notification_page.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -12,32 +15,60 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat App'),
         actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
-            ),
-            items: const [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.exit_to_app,
-                      color: Colors.black87,
-                    ),
-                    SizedBox(width: 10),
-                    Text('Sair'),
-                  ],
-                ),
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
               ),
-            ],
-            onChanged: (value) {
-              if (value == 'logout') {
-                AuthService().signout();
-              }
-            },
+              items: const [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Sair'),
+                    ],
+                  ),
+                ),
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().signout();
+                }
+              },
+            ),
           ),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (ctx) {
+                      return const NotificationPage();
+                    }),
+                  );
+                },
+                icon: const Icon(Icons.notifications),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
       body: const SafeArea(
