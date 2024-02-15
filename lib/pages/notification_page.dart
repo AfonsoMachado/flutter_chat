debdd8234a034b270840bf4efbbdbe5c38f6b521
Afonso Machado <afonsosmachado@gmail.com>
2024-02-15 11:43:39 -0300

    show notification on notification page

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/core/services/notification/chat_notification_service.dart';
+import 'package:provider/provider.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final service = Provider.of<ChatNotificationService>(context);
+    final items = service.items;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Minhas notificações'),
-      ),
-      body: Center(
-        child: Text('Notificações'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Minhas notificações'),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, i) => ListTile(
+            title: Text(items[i].title),
+            subtitle: Text(items[i].body),
+            onTap: () => service.remove(i),
+          ),
+          itemCount: service.itemsCount,
+        ));
   }
 }
