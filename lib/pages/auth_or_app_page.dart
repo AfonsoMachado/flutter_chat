b10ddd8c42459ccfe85f82fc1357f832b3be93fa
Afonso Machado <afonsosmachado@gmail.com>
2024-02-16 10:08:06 -0300

    initializing firebase application

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -4,23 +4,36 @@ import 'package:flutter_chat/core/services/auth/auth_service.dart';
 import 'package:flutter_chat/pages/auth_page.dart';
 import 'package:flutter_chat/pages/chat_page.dart';
 import 'package:flutter_chat/pages/loading_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  // Inicialização da aplicação firebase
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-        stream: AuthService().userChanges,
-        builder: (BuildContext context, AsyncSnapshot<ChatUser?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const LoadingPage();
-          } else {
-            return snapshot.hasData ? const ChatPage() : const AuthPage();
-          }
-        },
-      ),
+    return FutureBuilder(
+      future: init(context),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingPage();
+        } else {
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
+            builder: (BuildContext context, AsyncSnapshot<ChatUser?> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingPage();
+              } else {
+                return snapshot.hasData ? const ChatPage() : const AuthPage();
+              }
+            },
+          );
+        }
+      },
     );
   }
 }
