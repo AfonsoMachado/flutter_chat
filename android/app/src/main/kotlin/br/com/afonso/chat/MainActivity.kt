446436fcb9798fc6ac122f17d2ae4b7f6f07d5f4
Afonso Machado <afonsosmachado@gmail.com>
2024-02-16 14:32:53 -0300

    firebase configuration is ok

---
+++ android/app/src/main/kotlin/br/com/afonso/chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.afonso.chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/afonso/flutter_chat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.afonso.flutter_chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
