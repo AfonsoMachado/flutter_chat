5a570bbd4825ee12df600deecd8798b447ff0cc0
Afonso Machado <afonsosmachado@gmail.com>
2024-02-28 11:00:13 -0300

    create chat firebase service and saving message in the firesotre db

---
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_chat/core/models/chat_message.dart';
+import 'package:flutter_chat/core/models/chat_user.dart';
+import 'package:flutter_chat/core/services/chat/chat_service.dart';
+import 'dart:async';
+
+class ChatFirebaseService implements ChatService {
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return const Stream<List<ChatMessage>>.empty();
+  }
+
+  @override
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+
+    // ChatMessage => Map<String, dynamic>
+    store.collection('chat').add({
+      'text': text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': user.id,
+      'userName': user.name,
+      'userImageUrl': user.imageUrl,
+    });
+
+    return null;
+  }
+}
