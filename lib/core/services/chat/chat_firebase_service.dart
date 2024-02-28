2d41ab6dbfdb965d179f5cc2071217e49949aeb1
Afonso Machado <afonsosmachado@gmail.com>
2024-02-28 13:55:07 -0300

    retreving the messages from the server and correct bug on user signup

--- lib/core/services/chat/chat_firebase_service.dart
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -7,7 +7,30 @@ import 'dart:async';
 class ChatFirebaseService implements ChatService {
   @override
   Stream<List<ChatMessage>> messagesStream() {
-    return const Stream<List<ChatMessage>>.empty();
+    final store = FirebaseFirestore.instance;
+    final snapshots = store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .orderBy('createdAt', descending: true)
+        .snapshots();
+
+    return snapshots
+        .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
+
+    // Faz a mesma coisa que acima
+    // return Stream<List<ChatMessage>>.multi((controller) {
+    //   snapshots.listen((snapshot) {
+    //     for (final doc in snapshot.docs) {
+    //       // Convert the data stream in a list
+    //       List<ChatMessage> list =
+    //           snapshot.docs.map((doc) => doc.data()).toList();
+    //       controller.add(list);
+    //     }
+    //   });
+    // });
   }
 
   @override
